31a3fa73e610826b59f74f945f8c32df8e83cb46
brunnoterada <brunoterada99@gmail.com>
2021-06-23 10:14:06 -0300

    tyoe

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,26 +1,55 @@
 import React,{useState} from 'react';
-import { View, Text, TextInput } from 'react-native';
+
+//importação de itens nativos do react
+import { View, Text, Image, StatusBar } from 'react-native';
+
+//importação da página de estilo
 import { styles } from './style';
+
+//importação da imagem na inicial do aplicativo
+import IllustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/Buttonicon/index';
+
 export function SingIn(){
   /*Criando uma constante para atualiar o estado */
-  const[text,setText] =useState('');
+
 
   return(
+    
     <View style={styles.container}>
-      <Text>
-       olá
-      </Text>
-
-      <TextInput 
-      style={styles.input}
-      onChangeText={setText}
+      
+      {/* Status bar é a parte superiror
+      onde fica os status do seu telefone
+      nesse componente conseguimos ajustar 
+      para ficar visualmente mais trativo */}
+      <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
       />
-      <Text>
-        Você digitou: {text}
-
-      </Text>
+      
+      {/* Imagem do aplicativo */}
+      <Image 
+      source={IllustrationImg} 
+      style={styles.image}
+      resizeMode="stretch"
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize{`\n`}
+          suas jogatinas{`\n`}
+          facilmente{`\n`}
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{`\n`}
+          favoritos com seus amigos
+          
+        </Text>
+        <ButtonIcon 
+        title={'Entrar com Discoasrd'}
+        activeOpacity={0.5}
+        />
+      </View>
     </View>
-
-
   );
 }
