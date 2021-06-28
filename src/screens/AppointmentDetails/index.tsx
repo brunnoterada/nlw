7dd2982be7bed59db231725d883529c49e22b310
brunnoterada <brunoterada99@gmail.com>
2021-06-28 18:01:22 -0300

    arquivos atualizados

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -6,15 +6,16 @@ import {
   FlatList
 } from 'react-native';
 import { Fontisto } from "@expo/vector-icons";
-import {Background} from '../../components/Background/index';
+import { Background } from '../../components/Background/index';
 import { Header } from "../../components/Header";
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { theme } from "../../global/styles/theme";
 import BannerImg from '../../assets/banner.png';
 import { styles } from './styles';
+import { ListDivider } from "../../components/ListDivider";
 import { ListHeader } from "../../components/ListHeader";
 import { Member } from "../../components/Member";
-import { ListDivider } from "../../components/ListDivider";
+import { ButtonIcon } from "../../components/ButtonIcon";
 
 
 export function AppointmentDetails(){
@@ -81,6 +82,9 @@ export function AppointmentDetails(){
         style={styles.members}
 
       />
+      <View style={styles.footer}>
+      <ButtonIcon title="Entrar na partida"/>
+      </View>
       
 
 
